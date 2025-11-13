c280d6d8b9c3baf8f7e6f8143b92625267d9a04d
Hugo Desvignes <desvigneshugo05@gmail.com>
2025-11-13 13:28:44 +0700

    first commit, class book created

---
+++ oop_solution/library_oop.py
@@ -0,0 +1,9 @@
+class Book:
+    def __init__(self,id,title,author,total_copies,available_copies):
+        self.id = id
+        self.title = title
+        self.author = author
+        self.total_copies = total_copies
+        self.available_copies = available_copies
+
+
---
+++ oop_solution/test_oop.py
@@ -0,0 +1,4 @@
+from library_oop import *
+
+if __name__ == "__main__":
+    test_library_system()
